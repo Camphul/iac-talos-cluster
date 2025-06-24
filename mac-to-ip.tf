23deffd03f3433ddc63a884c776595555910da46
Luca Camphuisen <luca@logic-shift.com>
2025-06-24 08:05:56 +0200

    Comment out mac-to-ip data source and output in mac-to-ip.tf for future reference

--- mac-to-ip.tf
+++ mac-to-ip.tf
@@ -9,27 +9,27 @@ resource "time_sleep" "wait_for_vms_to_boot" {
 
 # vms are booted, use nmap to scan the network and match the known mac
 # addresses with the found ip addresses
-data "external" "mac-to-ip" {
-  depends_on = [time_sleep.wait_for_vms_to_boot]
+# data "external" "mac-to-ip" {
+#   depends_on = [time_sleep.wait_for_vms_to_boot]
+#
+#   program = concat([
+#     "go",
+#     "run",
+#     "${path.module}/cmd/mac-to-ip",
+#     "-subnet",
+#     join(",", var.mac-to-ip_scan_subnets),
+#     ],
+#     [for i, cfg in macaddress.talos-control-plane : cfg.address],
+#     [for i, cfg in macaddress.talos-worker-node : cfg.address],
+#   )
+# }
+#
+# output "mac-to-ip" {
+#   value = data.external.mac-to-ip.result
+# }
 
-  program = concat([
-    "go",
-    "run",
-    "${path.module}/cmd/mac-to-ip",
-    "-subnet",
-    join(",", var.mac-to-ip_scan_subnets),
-    ],
-    [for i, cfg in macaddress.talos-control-plane : cfg.address],
-    [for i, cfg in macaddress.talos-worker-node : cfg.address],
-  )
-}
-
-output "mac-to-ip" {
-  value = data.external.mac-to-ip.result
-}
-
-variable "mac-to-ip_scan_subnets" {
-  description = "Subnets to scan MAC addresses for IP addresses."
-  type        = list(string)
-  default     = ["10.0.0.1/24"]
-}
+# variable "mac-to-ip_scan_subnets" {
+#   description = "Subnets to scan MAC addresses for IP addresses."
+#   type        = list(string)
+#   default     = ["10.0.0.1/24"]
+# }
